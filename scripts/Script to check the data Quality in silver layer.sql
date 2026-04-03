028bdf0dbe3533a19d606e3d8f359b58a2baad2c
ThirumaranV337 <thirumarant337@gmail.com>
2026-04-03 16:44:37 +0530

    Enhance data quality checks in silver layer SQL script
    Added multiple checks for data quality in crm_sales_info and erp_cust_az12 tables, including invalid dates and consistency checks.

--- scripts/Script to check the data Quality in silver layer.sql
+++ scripts/Script to check the data Quality in silver layer.sql
@@ -50,3 +50,46 @@ from silver.crm_prd_info
 select * 
 from silver.crm_prd_info
 where prd_end_dt<prd_start_dt
+
+--==============================================================================
+--check for data quality in the crm_sales_info
+--==============================================================================
+
+--check for Invalid Dates
+SELECT
+NULLIF(sls_order_dt, 0) sls_order_dt
+FROM silver.crm_sales_info
+WHERE sls_order_dt <= 0
+OR LEN(sls_order_dt) != 8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+--checking for invalid date orders
+SELECT
+*
+FROM silver.crm_sales_info
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+/*
+Check Data Consistency : Between Sales, Quantity, and Price
+Sales = Quantity * Price
+Values must not be Null, Zero, or negative.
+*/
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_info
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+/*
+checking the quality of the erp_CUST_AZ12 TABLE 
+*/
+SELECT DISTINCT 
+bdate
+FROM silver.erp_cust_az12
+WHERE bdate <'1924-01-01' or bdate >GETDATE()
+--Data Standardization & consistency 
+SELECT DISTINCT gen 
+FROM silver.erp_cust_az12
